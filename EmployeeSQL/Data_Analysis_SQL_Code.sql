b5405f5e7648a3a2d2b84e7f4720e204b3472b0c
Michael Badinger <mcbadinger@gmail.com>
2020-11-15 16:25:46 -0600

    Question 1 done

--- EmployeeSQL/Data_Analysis_SQL_Code.sql
+++ EmployeeSQL/Data_Analysis_SQL_Code.sql
@@ -1,11 +1,17 @@
 -- 1) List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-
-
-
+Select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+From employees
+Inner Join salaries On
+employees.emp_no=salaries.emp_no
 
 
 -- 2) List first name, last name, and hire date for employees who were hired in 1986.
+
+
+
+
+
 
 
 -- 3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
