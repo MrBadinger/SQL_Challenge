dd0eabb5466447fc5401cd5a7e01711697222d1b
Michael Badinger <mcbadinger@gmail.com>
2020-11-15 16:40:18 -0600

    Question 2 done

--- EmployeeSQL/Data_Analysis_SQL_Code.sql
+++ EmployeeSQL/Data_Analysis_SQL_Code.sql
@@ -8,10 +8,10 @@ employees.emp_no=salaries.emp_no
 
 -- 2) List first name, last name, and hire date for employees who were hired in 1986.
 
-
-
-
-
+Select employees.first_name, employees.last_name, employees.hire_date
+From employees
+Where employees.hire_date >= '1986-01-01 00:00:00'
+AND employees.hire_date <  '1987-01-01 00:00:00'
 
 
 -- 3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
